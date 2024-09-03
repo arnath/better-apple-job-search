cf807f1bce7bc8e93fa51260c763e1933b68b6e9
Vijay Prakash <prakash.vijay@gmail.com>
2024-09-03 12:58:36 +0200

    More refactoring

--- fetcher/Program.cs
+++ fetcher/Program.cs
@@ -1,28 +1,27 @@
-﻿using BetterAppleJobSearch.Fetcher.Data;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json.Linq;
+﻿using Microsoft.Extensions.Logging;
 using OpenSearch.Net;
 
 namespace BetterAppleJobSearch.Fetcher;
 
 public class Program
 {
+    private static readonly LogLevel MinimumLogLevel = LogLevel.Information;
+
     public static async Task Main()
     {
         ILoggerFactory loggerFactory =
             LoggerFactory.Create((builder) =>
                 builder
                     .AddConsole()
-                    .SetMinimumLevel(LogLevel.Information)
+                    .SetMinimumLevel(MinimumLogLevel)
                     .AddFilter("Microsoft.EntityFrameworkCore.Database.Command", LogLevel.Warning));
+        ILogger logger = loggerFactory.CreateLogger<Program>();
 
         using AppleJobFetcher jobFetcher = new AppleJobFetcher(loggerFactory);
         List<dynamic> jobs = await jobFetcher.FetchAsync("./locations-tiny.json");
 
-        OpenSearchLowLevelClient openSearchClient = new OpenSearchLowLevelClient();
-        StringResponse openSearchResponse = await openSearchClient.BulkAsync<StringResponse>(PostData.MultiJson(jobs));
-        Console.WriteLine(openSearchResponse.HttpStatusCode);
-        Console.WriteLine(openSearchResponse.Body);
+        OpenSearchClient openSearchClient = new OpenSearchClient(loggerFactory, MinimumLogLevel);
+        await openSearchClient.PingAsync();
+        // await openSearchClient.IngestJobsAsync(jobs);
     }
 }
