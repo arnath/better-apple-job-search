4719eb04db0713a54e24578b16626ec121f3d5bd
Vijay Prakash <prakash.vijay@gmail.com>
2024-09-04 17:33:58 +0200

    Fix some bugs

--- fetcher/Sqlite/SqliteRepository.cs
+++ fetcher/Sqlite/SqliteRepository.cs
@@ -11,18 +11,22 @@ public class SqliteRepository(ILoggerFactory loggerFactory) : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options
-            .UseSqlite("Data Source=/Users/vijay/code/better-apple-job-search/db/jobs.db")
+            .UseSqlite("Data Source=/Users/vijay/code/better-apple-job-search/db/jobs-2024-09-01.db")
+            .UseSnakeCaseNamingConvention()
             .UseLoggerFactory(loggerFactory);
 
     public async Task InsertAppleJobsAsync(IEnumerable<dynamic> appleJobs)
     {
         foreach (dynamic jobData in appleJobs)
         {
+            // If this field doesn't exist, this assigns some default value to postDate.
+            DateTime.TryParse(jobData.postDateInGMT?.ToString(), out DateTime postDate);
+
             JobEntity job = new JobEntity()
             {
                 Id = $"apple_{jobData.id.ToString()}",
                 Link = new Uri($"https://jobs.apple.com/en-us/details/{jobData.positionId.ToString()}"),
-                PostDate = jobData.postDateInGMT as DateTime? ?? DateTime.MinValue,
+                PostDate = postDate,
                 Title = jobData.postingTitle.ToString(),
                 Summary = jobData.jobSummary.ToString(),
                 TeamName = jobData.team?.teamName?.ToString(),
